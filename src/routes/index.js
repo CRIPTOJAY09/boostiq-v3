147e292bf089f69e3c3f1581bc208e1fe8d80ac6
CriptoJay <91977435+CRIPTOJAY09@users.noreply.github.com>
2025-07-11 17:18:28 +0200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
-const setupTopGainers = require('./topGainers');
-const setupNewListings = require('./newListings');
+const setupTopGainers = require('./gainers');
+const setupNewListings = require('./listings');
 const setupAnalysis = require('./analysis');
-const setupExplosions = require('./explosionCandidates');
+const setupExplosions = require('./explosion');
+const setupAlerts = require('./alerts'); // Omitir si no lo estás usando
 
 module.exports = (app, CONFIG, shortCache, longCache) => {
   setupTopGainers(app, CONFIG, shortCache, longCache);
   setupNewListings(app, CONFIG, shortCache, longCache);
   setupAnalysis(app, CONFIG, shortCache, longCache);
   setupExplosions(app, CONFIG, shortCache, longCache);
+  setupAlerts(app, CONFIG, shortCache, longCache); // Omitir si no aplica
 };
